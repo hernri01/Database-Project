c251a30e41ae70413729078985de30b5890e8eec
hernri01 <40433585+hernri01@users.noreply.github.com>
2020-04-02 03:15:45 -0400

    Update appForm_f.php
    I added the ability to get the last application ID so that the user can remember in case they want to pull up their application.

--- appForm_f.php
+++ appForm_f.php
@@ -8,22 +8,46 @@ $submit = submitApp($db, $_POST);
 
 $name  = $_POST['name'];
 $eid   = $_POST['eid'];
-$dep  = $_POST['dep'];
-$country = $_POST['country'];
-$cty = $_POST['cty'];
-$sd = $_POST['sd'];
-$ed = $_POST['ed'];
-$budget = $_POST['budget'];
-
-
 ?>
 
 <HTML>
 <HEAD><TITLE>Thank you</TITLE></HEAD>
+<style>
+        html,body{
+            width: 100%;
+            height: 100%;
+            margin: 0; /* Space from this element (entire page) and others*/
+            padding: 0; /*space from content and border*/
+            border: solid blue;
+            border-width: 20px;
+            overflow:hidden;
+            display:block;
+            box-sizing: border-box;
+        }
+
+    </style>
 <BODY>
 <center><H2>Thank you for submitting your application.</H2>
 Your name :  <?php echo $name; ?><BR />
-Your Employee ID:  <?php echo $eid; ?><BR />
+Your Employee ID:  <?php echo $eid; ?> <BR />
+<strong>Your Application ID:  
+<?php
+    $qStr = "SELECT MAX(app_id) AS id FROM app";
+    $result = $db->query($qStr);
+
+	if($result != FALSE)
+	{	
+		while($row = $result->fetch()) {
+			$id = $row['id'];
+			print $id;	
+		}	
+	}
+?></strong> <br />
+
+<p> Please keep this <strong> Application ID </strong>number to check the status of your application. </p>
+<BR />
+
+
 <BR />
 
 Comment: <BR /> </center>
