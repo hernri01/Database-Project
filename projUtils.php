3886f2d174e9af23c5e65aa2f9e2d063181968f4
hernri01 <40433585+hernri01@users.noreply.github.com>
2020-04-02 03:14:25 -0400

    Update projUtils.php
    Added the view function so that you can pull up your application by your application ID. Future updates will be able to pull multiple applications with your employee id at sign in.

--- projUtils.php
+++ projUtils.php
@@ -9,10 +9,42 @@
         $startDate = $input['sd'];
         $endDate = $input['ed'];
         $budget = $input['budget'];
+        $country = $_POST['country'];
+        $cty = $_POST['cty'];
 
-        $qStr = "INSERT INTO app (emp_id, dno, loc, trip_start, trip_end, budget) VALUES ( '$emp', '$dno', '$location', '$startDate', '$endDate', '$budget');";
+        $qStr = "INSERT INTO app (emp_id, dno, country, city, trip_start, trip_end, budget) VALUES ( '$emp', '$dno', '$country', '$cty', '$startDate', '$endDate', '$budget');";
 
         $result = $db->query($qStr);
+    }
+
+    function view($db, $id)
+    {
+        $qStr = "SELECT * FROM app WHERE app_id = '$id';";
+
+        $result = $db->query($qStr);
+
+        if($result)
+        {
+            $rows = $result->rowCount();
+            if($rows == 0)
+            {
+                return -1;
+            }
+
+            while($row = $result->fetch()) 
+            {
+                $out['id'] = $row['emp_id'];
+                $out['dno'] = $row['dno'];
+                $out['country'] = $row['country'];
+                $out['city'] = $row['city'];
+                $out['sd'] = $row['trip_start'];
+                $out['ed'] = $row['trip_end'];
+                $out['budget'] = $row['budget'];
+
+                return $out;
+            }
+        
+        }
     }
 
 ?>
