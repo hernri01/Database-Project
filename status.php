c895c6f4a904f60c643b7e358a1b903e1402316b
Ricardo Hernandez <40433585+hernri01@users.noreply.github.com>
2020-04-07 20:22:14 -0400

    Update status.php

--- status.php
+++ status.php
@@ -1,3 +1,5 @@
+//This file is to just to change the status of the application. As of 4/07/20 only the manager can change the status of the application.
+//There is a basic message either saying the application has been approved or denied. Needs styling to look more professional.
 <?php 
 // Change the address of this when switching to the master webpage.
     include_once("db_connect.php");
