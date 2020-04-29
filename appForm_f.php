e7c850fd5f68dcb7f2a1aeb27aae334727be9c1d
Ricardo Hernandez <40433585+hernri01@users.noreply.github.com>
2020-04-29 09:57:47 -0400

    Add files via upload
    Redirects the user to the homepage after the information is submitted.

--- appForm_f.php
+++ appForm_f.php
@@ -7,53 +7,6 @@ session_start();
 
 $submit = submitApp($db, $_POST);
 
-$name  = $_POST['name'];
-$eid   = $_POST['eid'];
+header("Location: http://cs.gettysburg.edu/~hernri01/ProposalApp/landingPageEmp.php"); 
+
 ?>
-
-<HTML>
-<HEAD><TITLE>Thank you</TITLE></HEAD>
-<style>
-        html,body{
-            width: 100%;
-            height: 100%;
-            margin: 0; /* Space from this element (entire page) and others*/
-            padding: 0; /*space from content and border*/
-            border: solid blue;
-            border-width: 20px;
-            overflow:hidden;
-            display:block;
-            box-sizing: border-box;
-        }
-
-    </style>
-<BODY>
-<center><H2>Thank you for submitting your application.</H2>
-Your name :  <?php echo $name; ?><BR />
-Your Employee ID:  <?php echo $eid; ?> <BR />
-<strong>Your Application ID:  
-<?php
-    $qStr = "SELECT MAX(app_id) AS id FROM app";
-    $result = $db->query($qStr);
-
-	if($result != FALSE)
-	{	
-		while($row = $result->fetch()) {
-			$id = $row['id'];
-			print $id;	
-		}	
-	}
-?></strong> <br />
-
-<p> Please keep this <strong> Application ID </strong>number to check the status of your application. </p>
-<BR />
-
-
-<BR />
-
-Comment: <BR /> </center>
-
-
-
-</BODY>
-</HTML>
