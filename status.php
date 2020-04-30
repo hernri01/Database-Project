ac2fd6e3beec168ae79d1b990d6f79d94ca885b3
Ricardo Hernandez <40433585+hernri01@users.noreply.github.com>
2020-04-29 20:59:13 -0400

    Add files via upload

--- status.php
+++ status.php
@@ -10,10 +10,21 @@
 
     if(($status == 2) || ($status == 4)) // It was approved. 
     {
-        echo "<center><h1> Application Approved. </h1></center>";
+        if($status == 2)
+        {
+            header("Location: http://cs.gettysburg.edu/~hernri01/ProposalApp/landingPageMgr.php"); 
+
+        }
+        else 
+        {
+            header("Location: http://cs.gettysburg.edu/~hernri01/ProposalApp/landingPageExec.php"); 
+
+        }
     }
     else if(($status == 1 )|| ($status == 3)) // denied
     {
+        //Hello Oriin, This the code that needs styling. The code above will redirect to the home pages. 
+        //It needs to look like every other page and navigation bar. 
         echo "<center><h1> Application Denied. </h1></center>
             <form method='POST' action='feedback.php?id=$id'>
             <center><textarea placeholder = 'Please provide feedback' name='message' rows='15' cols='40'></textarea></center><br>
