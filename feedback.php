95a39d8c37f8506a54167a58330e4be7fab400a8
Ricardo Hernandez <40433585+hernri01@users.noreply.github.com>
2020-04-29 20:55:17 -0400

    Add files via upload

--- feedback.php
+++ feedback.php
@@ -1,15 +1,27 @@
-//This file is just the text box to provide feedback. 
 <?php
 require_once("db_connect.php");
 require_once("projUtils.php");
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+session_start();
+
 
 $message = $_POST['message'];
 $id = $_GET['id'];
+$user = $_SESSION['type'];
 
 sendFeedback($db,$message,$id);
 
-echo "<center><h1> Thank you! </h1></center>";
+//Depending on the user, it will redirect to their respective home page.
+if($user == 'mgr')
+{
+    header("Location: http://cs.gettysburg.edu/~hernri01/ProposalApp/landingPageMgr.php"); 
+}
+else 
+{
+    header("Location: http://cs.gettysburg.edu/~hernri01/ProposalApp/landingPageExec.php");
+}
+
+
 ?>
